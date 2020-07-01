ad9ce8a1af6a6110d92a1805a0c66bb5cd80991e
Gun Eight <guneight7@gmail.com>
2020-07-01 20:28:28 +0700

    Initial Commit

---
+++ AloMobileTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AloMobileTest
+//
+//  Created by Gun Eight  on 01/07/20.
+//  Copyright © 2020 Gun Eight . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
